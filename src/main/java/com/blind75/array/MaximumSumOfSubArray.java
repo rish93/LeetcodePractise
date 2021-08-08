e52d25a5c445ecc0b6b33d4e793fa2711cf23b90
Mukherjee <dibyendu.mukherjee@infraviw.net>
2021-08-08 21:56:50 +0530

    sunday

---
+++ src/main/java/com/blind75/array/MaximumSumOfSubArray.java
@@ -0,0 +1,74 @@
+package com.blind75.array;
+
+/*
+Given an integer array nums, find the contiguous subarray
+(containing at least one number) which has the largest sum and return its sum.
+
+A subarray is a contiguous part of an array.
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1]
+has the largest sum = 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+ */
+
+public class MaximumSumOfSubArray {
+
+    public static void main(String[] args) {
+        System.out.println(containsMaximumSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));  //6
+        System.out.println(containsMaximumSubArray(new int[]{1})); //1
+        System.out.println(containsMaximumSubArray(new int[]{5, 4, -1, 7, 8})); //23
+        System.out.println(containsMaximumSubArray(new int[]{-1})); //-1
+        System.out.println(containsMaximumSubArray(new int[]{-2, -1})); //-1
+        System.out.println(containsMaximumSubArray(new int[]{-2, 1})); //1
+
+
+        System.out.println(kadaneAlgorithm(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));  //6
+        System.out.println(kadaneAlgorithm(new int[]{1})); //1
+        System.out.println(kadaneAlgorithm(new int[]{5, 4, -1, 7, 8})); //23
+        System.out.println(kadaneAlgorithm(new int[]{-1})); //-1
+        System.out.println(kadaneAlgorithm(new int[]{-2, -1})); //-1
+        System.out.println(kadaneAlgorithm(new int[]{-2, 1})); //1
+
+    }
+
+    public static Integer containsMaximumSubArray(int[] nums) {
+        int pointer_b = 0;
+        int pointer_a = 0;
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        while (pointer_b < nums.length) {
+            sum += nums[pointer_b];
+            if (sum > max) {
+                max = sum;
+
+            }
+            if (sum < 0) {
+                sum = 0;
+                pointer_a++;
+            }
+            pointer_b++;
+        }
+        return max;
+
+    }
+
+
+    public static int kadaneAlgorithm(int[] nums) {
+        int maxSoFar = nums[0], maxEndingHere = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+        return maxSoFar;
+    }
+}
