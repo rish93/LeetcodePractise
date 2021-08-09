902052bebf62a011ef9f0d9554b812a156d35c80
Mukherjee <dibyendu.mukherjee@infraviw.net>
2021-08-09 22:44:42 +0530

    monday linklist

--- src/main/java/com/blind75/linkedlist/ReverseLinkedList.java
+++ src/main/java/com/blind75/linkedlist/ReverseLinkedList.java
@@ -1,38 +1,98 @@
+package com.blind75.linkedlist;/*
 package com.blind75.linkedlist;
 
 public class ReverseLinkedList {
-    public static void main(String []args) {
+    public static void main(String[] args) {
 
         //create linked list
-
-        ListNode node1= new ListNode(5);
-
-        ListNode node2= new ListNode(10,node1);
-
-        ListNode node3= new ListNode(20,node2);
+        //20->10->5->null
+        int[] arr = new int[]{1, 2, 3, 4, 5};
+        Node head = new Node(arr[arr.length - 1]);
+        for (int i = arr.length - 2; i >= 0; i--) {
+            head = new Node(arr[i], head);
+        }
         Solution solution = new Solution();
-       ListNode listNodeReverse= solution.reverseList(node3);
+        Node nodeReverse = solution.reverseList(head);
     }
 
 }
 
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
+    public Node reverseList(Node head) {
+        Node prev = null;
+        Node curr = head;                //1
         while (curr != null) {
-            ListNode nextTemp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextTemp;
+            Node nextTemp = curr.next;      //2
+            curr.next = prev;              //null
+            prev = curr;                   //1
+            curr = nextTemp;               // 2
         }
         return prev;
     }
 }
- class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+
+class Node {
+    int val;
+    Node next;
+
+    Node() {
+    }
+
+    Node(int val) {
+        this.val = val;
+    }
+
+    Node(int val, Node next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+*/
+
+
+public class ReverseLinkedList {
+    public static void main(String[] args) {
+        int[] inputArray = new int[]{1, 2, 3, 4, 5};
+
+        Node head = new Node(inputArray[inputArray.length - 1]);
+        for (int i = inputArray.length - 2; i >= 0; i--) {
+            head = new Node(inputArray[i], head);
+        }
+        Node output = reverseLinkedList(head);
+        while (output.next != null) {
+            System.out.println(output.value);
+            output=output.next;
+        }
+
+    }
+
+    static Node reverseLinkedList(Node head) {
+        Node prev= null;
+        while (head.next!=null)
+        {
+            Node nextNode = head.next;
+            head.next=prev;
+            prev=head;       //1 step
+            head=nextNode;  //2nd step
+        }
+        return prev;
+    }
+
+}
+
+
+class Node {
+    int value;
+    Node next;
+
+    Node(int value) {
+        this.value = value;
+    }
+
+    Node(int value, Node next) {
+        this.value = value;
+        this.next = next;
+    }
+
+}
+
