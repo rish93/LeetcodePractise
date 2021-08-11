b5e4267cb6e4beb742bca8699df38b634cf5683b
Mukherjee <dibyendu.mukherjee@infraviw.net>
2021-08-12 00:27:55 +0530

    thursday

--- src/main/java/com/blind75/string/ValidParentheses.java
+++ src/main/java/com/blind75/string/ValidParentheses.java
@@ -1,23 +1,34 @@
 package com.blind75.string;
 
+import java.util.Stack;
+
 public class ValidParentheses {
     public static void main(String[] args) {
 
         String s = "()[]{}";
+        String s1 = "({[(}]}){}";
+        String s2 = "({[(])}){}";
+        System.out.println("OUTPUT->  " + isValid(s1));
+        System.out.println("OUTPUT->  " + isValid(s2));
         System.out.println("OUTPUT->  " + isValid(s));
     }
 
-
     public static boolean isValid(String s) {
-        int pointer_a = 0;
-        int freq = 0;
-        for (Character c : s.toCharArray()) {
-            freq = c ;
-            System.out.println(freq);
+        Stack<Character> stack = new Stack<>();
+        if (s.length() % 2 != 0)
+            return false;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[')
+                stack.push(c);
+            if (c == ')' && !stack.empty() && stack.peek() == '(')
+                stack.pop();
+            if (c == '}' && !stack.empty() && stack.peek() == '{')
+                stack.pop();
+            if (c == ']' && !stack.empty() && stack.peek() == '[')
+                stack.pop();
         }
-
-
-        return false;
+        return stack.isEmpty();
     }
 }
 
