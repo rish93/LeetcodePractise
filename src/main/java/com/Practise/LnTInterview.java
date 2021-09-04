82f56c12645eb130b2461bf070fee54545fd5bf1
rish93 <rish93@gmail.com>
2021-09-04 21:35:11 +0530

    added lnt and societe generale

---
+++ src/main/java/com/Practise/LnTInterview.java
@@ -0,0 +1,51 @@
+package com.Practise;
+
+public class LnTInterview {
+
+    //Builder design pattern
+
+    //thread join vs wait
+
+    //yield method in thread
+
+    //class not found exception vs class defnnotfound exception
+
+    //spring cloud what is service discovery how handled
+
+    //how to handle hashing collision
+
+    //how hashing works for HashSet
+
+    //why order not maintained in hashset
+
+    //make class like string
+
+    //hibernate session and it's type
+
+    //hibernate save vs saveOrUpdate
+
+    //how to make project enabling repository what all is needed
+
+    //what are streams/lambda expression
+
+    //how to access inner class variables
+
+    //type of inner class
+
+    //diff between inner class and lamba expression
+
+    //what is try with resource from java 7
+
+    //checked vs unchecked exeption
+
+    //custom exception
+
+    //deep cloning shallow cloning
+
+    //when have String s= new String("rishabh") //will it create in string pool
+
+    //return type matters in overloading /overriding
+
+    //oops concept with eg
+
+}
---
+++ src/main/java/com/Practise/SocieteGeneraleInterview.java
@@ -0,0 +1,34 @@
+package com.Practise;
+
+public class SocieteGeneraleInterview {
+
+
+    //what design pattern JPA uses
+
+    //what design pattern you have worked on
+
+    //sort the aray even first odd last
+
+    //find element of array which is sum of two input n, m
+
+    //suppose we have sum now, find two element which sums to it
+
+    //oops concept
+
+    //if we have default method in interface will it need to be implemented with implementation classes
+
+    //why interface whn have abstract class
+
+    //string buffer s1 = new String ("abhi") == new String buffer s2("abhi")  will it be same
+
+    //is string class thread safe
+
+    //what is marker interface , functional interface
+
+    //make a setup of spring boot for student table crud operations
+
+    //equals method of objct class checks reference or value
+
+    //worked on servlet
+
+}
