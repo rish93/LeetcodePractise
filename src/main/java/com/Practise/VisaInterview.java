aa4ef2ac8a5695962c249741661de18d96b67499
Rishabh <rishabh.mankatala@infraview.net>
2021-10-13 09:46:17 +0530

    added visa question

---
+++ src/main/java/com/Practise/VisaInterview.java
@@ -0,0 +1,89 @@
+package com.Practise;
+
+public class VisaInterview {
+
+    /*
+    * Round 1:
+Q1) how to add time taken by an endpoint without adding calculation logic in every controller method.
+Q2) what is the n+1 problem in hibernate? how is it resolved?
+Q3) 1 coding question: https://www.geeksforgeeks.org/find-maximum-meetings-in-one-room/
+Q4) Design a Youtube
+Answer Question
+Round 3:
+Q1) Count the number of occurrences (or frequency) of a number in a sorted array.
+Solution: https://www.geeksforgeeks.org/count-number-of-occurrences-or-frequency-in-a-sorted-array/
+Q2) Design a TinyUrl System
+Answer Question
+Round 5:
+This was an offline round. 3 question were shared on mail and had to revert with a solution.
+
+1. Algo question:
+
+· N people, M restaurants, on a one-dimensional street. All the people are to meet at one restaurant, find the restaurant, where total travel distance from all people to that restaurant is the shortest among all restaurants.
+
+· The problem can be solved with brutal force with O(n*m) complexity. The question is how to solve it faster than brutal force.
+
+· Describe the algorithm, and why it is better than brutal force. Coding is not required
+
+2. Coding question:
+
+· Binary tree in memory (not sorted, not balanced), write code to serialize the binary tree to disk, then deserialize from disk and re-construct in memory.
+
+· Runnable code is required. You may use any language you like. You may also decide the format on disk.
+
+3. System design:
+
+· 1 million backend server that serves search traffic (think it as search service by google).
+
+· 10k frontend servers – the FE servers intend to act as load balance the incoming traffic to the BE servers.
+
+· Incoming traffic to the 10k FE servers are not balanced: there can be cold FE servers and hot FE servers
+
+· Design the system/algorithm, where the traffic to BE servers are balanced.
+
+· The design should be within today’s computer architecture, ideally without centralized components.
+    *
+    * Finding the longest palindromic substring in a given string
+    *
+    *
+    * Round 1
+==========
+1. Count minimum number of fountains to be activated to cover the entire garden
+2. Minimum possible sum of array elements after performing the given operation
+
+Round 2
+================
+1.Reverse an Integer
+2.Implement LRU cache
+3.How can you reduce shuffling in spark
+4. What are the various standards for Micro Service ?
+5.Difference between REST and SOAP?
+
+
+Round 2
+=================
+1.Implement HashMap
+2. Find nearest Cofee Machine
+3.Explain structure of parquet file
+4.What is run length encoding in parquet file
+5.Which compression technique you use
+6.What is the advantage of splittable in hadoop
+7.What information does memory of namenode carry
+8.What mechanism does spark use to communicate between Driver and Executor ?
+
+Round 3
+===========================
+1.Find Max Depth of Binary Tree
+2.Coin Change
+3.How HA is achieved in Hadoop
+4.What is the function of Zookeeper
+5..Have you used Spark Streaming
+
+Round 4
+==============
+Design Youtube
+    *
+    * >Design Need for Speed
+>Design HeatMap
+    * */
+}
