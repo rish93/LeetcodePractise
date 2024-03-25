9c0b52cd0f26bb819adbf4776d1ad2aafc846880
Rishabh Mankatala <rishabh.mankatala@boeing.com>
2024-03-25 14:02:04 +0530

    distributed system design

--- src/main/java/com/SystemDesign/DistributedCounterSystemDesign.java
+++ src/main/java/com/SystemDesign/DistributedCounterSystemDesign.java
@@ -3,4 +3,7 @@ package com.SystemDesign;
 public class DistributedCounterSystemDesign {
 
 //https://systemdesign.one/distributed-counter-system-design/
+
+    //similar questions
+    //Build a system to distribute 6 million free burgers in ~10 minutes.
 }
