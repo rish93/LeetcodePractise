81beaf4748edef23ae6873423545e931d3938216
Rishabh Mankatala <rishabh.mankatala@boeing.com>
2024-10-22 13:53:16 +0530

    added merge string alternatively

--- src/main/java/com/blind75/string/MergeStringAlternately_1768.java
+++ src/main/java/com/blind75/string/MergeStringAlternately_1768.java
@@ -43,6 +43,82 @@ public class MergeStringAlternately_1768 {
     * */
 
     public static void main(String[] args) {
+        String word1 = "abc", word2 = "pqr";
+        //Output: "apbqcr"
+        System.out.println(mergeAlternately(word1, word2));
+
+        String s1 = "abcd", s2 = "pq";
+        /*
+        *
+          Output: "apbqcd"
+          Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+          word1:  a   b   c   d
+          word2:    p   q
+          merged: a p b q c   d
+        * */
+        System.out.println(mergeAlternately(s1, s2));
+
+
+
+        String s3="cdf"; String s4 ="a";
+        /**
+         * o/p "cadf"
+         * */
+        System.out.println(mergeAlternately(s3, s4));
 
     }
-}
+
+    public static String mergeAlternately(String word1, String word2) {
+            int word1Length= word1.length();
+            int word2Length= word2.length();
+         //   int i = 0, j=0;
+            StringBuilder result= new StringBuilder();
+            if(word1Length>0 && word2Length>0){
+                for (int i = 0; i < word1Length || i < word2Length; ++i) {
+                    if (i < word1Length) {
+                        result.append(word1.charAt(i));
+                    }
+                    if (i < word2Length) {
+                        result.append(word2.charAt(i));
+                    }
+                }
+
+                /*if(word1Length>word2Length) {
+                    int diffLength = word1Length - word2Length;
+                    while (i < word2Length) {
+                        if(word1Length<2)
+                            result.append(word1.charAt(i));
+                        if(word2Length<2)
+                            result.append(word2.charAt(i));
+                        i++;
+                    }
+                    result.append(result, diffLength, word1Length);
+                  } else if(word2Length>word1Length){
+                        int diffLength = word2Length - word1Length;
+                        while (i < word1Length) {
+                            if(word1Length<2)
+                               result.append(word1.charAt(i));
+                            if(word2Length<2)
+                                result.append(word2.charAt(i));
+                            i++;
+                        }
+                        result.append(result, diffLength, word2Length);
+
+                } else if(word1Length == word2Length){
+
+                    //equal string
+                    while(i<word1Length){
+
+                        result.append(word1.charAt(i));
+                        result.append(word2.charAt(i));
+                        i++;
+                    }
+
+                }
+                */
+
+            }
+            return result.toString();
+          }
+    }
+
