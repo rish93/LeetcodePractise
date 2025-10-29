fbea4800a779741dae783016311011c8fc1f95c0
rish93 <rish93@gmail.com>
2025-10-29 19:41:22 +0530

    added albertson question

---
+++ src/main/java/com/Practise/AlbertsonsInternational.java
@@ -0,0 +1,25 @@
+package com.Practise;
+
+public class AlbertsonsInternational {
+
+    ///each service interacting with other how to trace its each of the logs //correlationId
+    //if log level is set to error which of the log wiill be vidsible..
+
+    //how to debug deadlock
+    //kafka if broker had  high load,,
+//    uf load increase in server api what will you do at coade level,
+
+    //what to review in code
+
+    //pool database in spring boot
+
+    //automatic commit vs manual...
+
+    //how to handle old data backeup in mongo
+
+
+
+    //fidn chaarcter count in string
+    //array of element , sort such that 1st max, 1min , then seiocnd max, seocnd min,
+    // thrid max and thiordmin ... are placed in array
+}
